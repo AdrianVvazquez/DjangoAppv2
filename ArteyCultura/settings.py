43ce6a884a0c8181e7fbe80c5a627220507cfa39
AdrianVvazquez <josea.vazquez@edu.uag.mx>
2021-07-23 19:37:41 -0500

    Nuevos archivos

--- ArteyCultura/settings.py
+++ ArteyCultura/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_HOST = "smtp.googlemail.com"
 EMAIL_PORT = "587"
