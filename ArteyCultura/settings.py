9a7257dae0a0dd88ce50281a42cb55619db6c578
AdrianVvazquez <josea.vazquez@edu.uag.mx>
2021-07-26 16:37:26 -0500

    Nuevo install

--- ArteyCultura/settings.py
+++ ArteyCultura/settings.py
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ArteyCultura.urls'
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static','static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 EMAIL_HOST = "smtp.googlemail.com"
